fbdadf4afa58d690fc2b2c8fb13d338952bca362
juand <juandiegoparrac@gmail.com>
2026-08-17 17:50:05 -0500

    Se elimino el archivo temporal y se agrego constantes base del shell(utils.h)

---
+++ user/utils.h
@@ -0,0 +1,8 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#define MAXLINE   128   // longitud maxima de la linea leida por el usuario
+#define MAXARGS   10    // maximo de argumentos por comando (incluye argv[0])
+#define MAXSTAGES 10    // maximo de comandos encadenados por tuberias
+
+#endif
