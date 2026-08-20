39cef8f3a5b5a427efd142565fdc918423dd6fa0
Samuel Molina Garcés <samuelmolina871@gmail.com>
2026-08-20 16:32:39 -0500

    Se actualiza la condicion que procesa el comando "exit" (sh.c)

--- user/sh.c
+++ user/sh.c
@@ -5,6 +5,7 @@
 #include "parser.h"
 #include "commands.h"
 
+// Funcion para asegurar que que la shell tenga sus fd's predeterminados al iniciar su ejecucion
 static void ensure_std_fds(void) {
   int fd;
   while ((fd = open("console", O_RDWR)) >= 0) {
@@ -15,6 +16,7 @@ static void ensure_std_fds(void) {
   }
 }
 
+// Funcion principal con ciclo REPL (Read, Eval, Print Loop)
 int main(void) {
   printf("\n>>> SOY LA NUEVA SHELL <<<\n\n");
   static char line[MAXLINE];
@@ -26,22 +28,25 @@ int main(void) {
     memset(line, 0, sizeof(line));
     gets(line, sizeof(line));
 
+    // Condicion por si el usuario no ingresa nada por teclado
     if (line[0] == 0)
       break;
 
+    // Bloque para limpiar los saltos de linea \n y \r
     int len = strlen(line);
     while (len > 0 && (line[len - 1] == '\n' || line[len - 1] == '\r')) {
       line[len - 1] = '\0';
       len--;
     }
 
+    // Bloque de analisis lexico y sintactico
     struct pipeline pl;
     int r = parse_line(line, &pl);
     if (r <= 0)
       continue;
 
-    if (pl.nstages == 1 && pl.stages[0].argc >= 1 &&
-        strcmp(pl.stages[0].argv[0], "exit") == 0) {
+    // Condicion para procesar el comando "exit"
+    if (pl.nstages == 1 && pl.stages[0].argc >= 1 && strcmp(pl.stages[0].argv[0], "exit") == 0) {
       free_pipeline(&pl);
       exit(0);
     }
